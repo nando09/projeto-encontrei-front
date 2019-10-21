c46778ce0a8679796ae837a652c98a7d1feb8c18
Renan Fernandes da Silva <renan@weblo.com.br>
2019-10-21 15:22:19 -0300

    Fixed router links and created "Admin Cadaster" page

---
+++ src/Pages/CadasterAdmin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="auth-fluid-pages cadasterPage pb-0">
+    <CadasterContent/>
+  </div>
+</template>
+
+
+<script>
+    import CadasterContent from "@/components/contents/CadasterContent"
+
+    export default {
+        name: 'Cadastro',
+        data () {
+            return {
+
+            }
+        },
+        components:{
+            CadasterContent
+        }
+    }
+</script>
+
+<style>
+
+</style>
