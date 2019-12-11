5dced66c4735c33ba42b570b5cbb12a505c87917
Fernando <fernando@weblo.com.br>
2019-12-11 15:18:16 -0300

    Load e alterações com o sucesso

--- src/Pages/Admin/Cadaster/ProductsService.vue
+++ src/Pages/Admin/Cadaster/ProductsService.vue
@@ -70,108 +70,109 @@
 						</div>
 						<!-- end col -->
 					</div>
-		  <div class="row">
-			<div class="col-xl-12">
-			  <div class="card">
-				<div class="card-body">
-				  <div class="table-responsive-sm">
-					<table class="table table-centered mb-0">
-					  <thead>
-						<tr>
-						  <th width="40%">Nome</th>
-						  <th width="40%">Preço</th>
-						  <th>Ação</th>
-						</tr>
-					  </thead>
-					  <tbody>
-						<tr v-for="item in items">
-						  <td>{{ item.nome }}</td>
-						  <td>{{ formatPrice(item.preco) }}</td>
-						  <td>
-							<button
-							  @click="editarProduto(item.id)"
-							  class="btn btn-sm btn-info mr-2"
-							  data-toggle="modal"
-							  data-placement="top"
-							  title
-							  data-original-title="Editar"
-							  type="button"
-							  data-target="#modalEdit"
-							>
-							  <i class="dripicons-document-edit"></i>
-							</button>
-							<button
-							  @click="deleteProduto(item.id)"
-							  class="btn btn-sm btn-danger"
-							  data-toggle="tooltip"
-							  data-placement="top"
-							  title
-							  data-original-title="Excluir"
-							>
-							  <i class="dripicons-cross"></i>
-							</button>
-						  </td>
+					<div class="row">
+						<div class="col-xl-12">
+							<div class="card">
+								<div class="card-body">
+									<div class="table-responsive-sm">
+									<table class="table table-centered mb-0">
+										<thead>
+										<tr>
+											<th width="40%">Nome</th>
+											<th width="40%">Preço</th>
+											<th>Ação</th>
+										</tr>
+										</thead>
+										<tbody>
+										<tr v-for="item in items">
+											<td>{{ item.nome }}</td>
+											<td>{{ formatPrice(item.preco) }}</td>
+											<td>
+											<button
+												@click="editarProduto(item.id)"
+												class="btn btn-sm btn-info mr-2"
+												data-toggle="modal"
+												data-placement="top"
+												title
+												data-original-title="Editar"
+												type="button"
+												data-target="#modalEdit"
+											>
+												<i class="dripicons-document-edit"></i>
+											</button>
+											<button
+												@click="deleteProduto(item.id)"
+												class="btn btn-sm btn-danger"
+												data-toggle="tooltip"
+												data-placement="top"
+												title
+												data-original-title="Excluir"
+											>
+												<i class="dripicons-cross"></i>
+											</button>
+											</td>
 
-						  <div
-							class="modal fade"
-							id="modalEdit"
-							tabindex="-1"
-							role="dialog"
-							aria-labelledby="exampleModalLabel"
-							aria-hidden="true"
-						  >
-							<div class="modal-dialog" role="document">
-							  <div class="modal-content">
-								<div class="modal-header">
-								  <h5 class="modal-title" id="exampleModalLabel">Editar Produto</h5>
-								  <button
-									type="button"
-									class="close"
-									data-dismiss="modal"
-									aria-label="Fechar"
-								  >
-									<span aria-hidden="true">&times;</span>
-								  </button>
-								</div>
-								<div class="modal-body">
-								  <form action class="form-group">
-									<div class="col-12 d-inline-flex">
-									  <div class="col-6">
-										<label for>Nome</label>
-										<input type="text" class="form-control" v-model="updateNome" />
-									  </div>
-									  <input type="hidden" v-model="updateId" />
-									  <div class="col-6">
-										<label for>Preço</label>
-										<input type="text" class="form-control" v-model="updatePreco" v-mask-decimal.br="2"/>
-									  </div>
+											<div
+											class="modal fade"
+											id="modalEdit"
+											tabindex="-1"
+											role="dialog"
+											aria-labelledby="exampleModalLabel"
+											aria-hidden="true"
+											>
+											<div class="modal-dialog" role="document">
+												<div class="modal-content">
+												<div class="modal-header">
+													<h5 class="modal-title" id="exampleModalLabel">Editar Produto</h5>
+													<button
+													type="button"
+													class="close"
+													data-dismiss="modal"
+													aria-label="Fechar"
+													>
+													<span aria-hidden="true">&times;</span>
+													</button>
+												</div>
+												<div class="modal-body">
+													<form action class="form-group">
+													<div class="col-12 d-inline-flex">
+														<div class="col-6">
+														<label for>Nome</label>
+														<input type="text" class="form-control" v-model="updateNome" />
+														</div>
+														<input type="hidden" v-model="updateId" />
+														<div class="col-6">
+														<label for>Preço</label>
+														<input type="text" class="form-control" v-model="updatePreco" v-mask-decimal.br="2"/>
+														</div>
+													</div>
+													</form>
+												</div>
+												<div class="modal-footer">
+													<button
+													type="button"
+													class="btn btn-secondary"
+													data-dismiss="modal"
+													>Fechar</button>
+													<button
+													type="button"
+													class="btn btn-primary"
+													data-dismiss="modal"
+													@click="updateProduto()"
+													>Salvar mudanças</button>
+												</div>
+												</div>
+											</div>
+											</div>
+										</tr>
+										</tbody>
+									</table>
 									</div>
-								  </form>
 								</div>
-								<div class="modal-footer">
-								  <button
-									type="button"
-									class="btn btn-secondary"
-									data-dismiss="modal"
-								  >Fechar</button>
-								  <button
-									type="button"
-									class="btn btn-primary"
-									data-dismiss="modal"
-									@click="updateProduto()"
-								  >Salvar mudanças</button>
-								</div>
-							  </div>
 							</div>
-						  </div>
-						</tr>
-					  </tbody>
-					</table>
-				  </div>
-				</div>
-			  </div>
-			</div>
-		  </div>
+						</div>
+					</div>
+					<!-- <button @click="showAlert">Hello world</button> -->
 				</div>
 				<!-- container -->
 			</div>
@@ -225,20 +226,20 @@ export default {
 				provider_id:	this.id,
 			};
 
-			// // console.log(data);
-
-			// axios.post('http://localhost:8000/api/produto', data, {
-			axios
-				.post("https://service.encontrei.online/api/produto", data, {
+			axios.post("https://service.encontrei.online/api/produto", data, {
 					headers: {
 						"Content-Type": "application/json",
 						Authorization: "Bearer " + this.user.token
 					}
 				})
 				.then(response => {
-					// console.log(response.data);
-					alert(response.data.nome + " cadastrado com sucesso!");
-					this.responsavel = response.data;
+					if (response.data.id){
+						this.toast(response.data.nome + ' cadastrado com sucesso!');
+					}else{
+						this.toast('Preecher todos os campos!');
+					}
+
+					// this.responsavel = response.data;
 					this.changeProduct();
 
 					this.nome = "";
@@ -250,23 +251,58 @@ export default {
 				});
 		},
 
+		toast(nome){
+			const Toast = this.$swal.mixin({
+				toast: true,
+				position: 'top-end',
+				showConfirmButton: false,
+				timer: 3000,
+				timerProgressBar: true,
+				onOpen: (toast) => {
+					toast.addEventListener('mouseenter', this.$swal.stopTimer)
+					toast.addEventListener('mouseleave', this.$swal.resumeTimer)
+				}
+			});
+
+			Toast.fire({
+				icon: 'success',
+				title: nome
+			});
+		},
+
 		deleteProduto(id) {
-			console.log(id);
 			// axios.delete('http://localhost:8000/api/produto/' + id, {
-			axios
-				.delete("https://service.encontrei.online/api/produto/" + id, {
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "Bearer " + this.user.token
-					}
-				})
-				.then(response => {
-					alert(response.data.nome + " deletado com sucesso!");
-					this.changeProduct();
-				})
-				.catch(e => {
-					alert("servidor fora de área");
-				});
+
+			this.$swal.fire({
+				title: 'Você tem certeza?',
+				text: "Você não poderá reverter isso!",
+				icon: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#3085d6',
+				cancelButtonColor: '#d33',
+				confirmButtonText: 'Sim, apague!'
+			}).then((result) => {
+				if (result.value) {
+					axios.delete("https://service.encontrei.online/api/produto/" + id, {
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: "Bearer " + this.user.token
+						}
+					})
+					.then(response => {
+						this.$swal.fire(
+							'Excluída!',
+							'Seu produto ' + response.data.nome + ' foi deletado.',
+							'success'
+						);
+
+						this.changeProduct();
+					})
+					.catch(e => {
+						alert("servidor fora de área");
+					});
+				}
+			})
 		},
 
 		updateProduto() {
@@ -275,18 +311,23 @@ export default {
 				preco: this.updatePreco
 			};
 			let id = this.updateId;
-			axios
-				.put("https://service.encontrei.online/api/produto/" + id, data, {
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: "Bearer " + this.user.token
-					}
-				})
-				.then(res => {
-					this.changeProduct();
-					// alert("Produto alterado com sucesso.");
-				})
-				.catch(e => alert(e));
+
+			axios.put("https://service.encontrei.online/api/produto/" + id, data, {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: "Bearer " + this.user.token
+				}
+			})
+			.then(res => {
+				this.changeProduct();
+				if (res.data.id) {
+					this.toast(res.data.nome + " alterado com sucesso!");
+				}else{
+					this.toast('Preecher todos os campos!');
+				}
+				// alert("Produto alterado com sucesso.");
+			})
+			.catch(e => alert(e));
 		},
 
 		editarProduto(id) {
@@ -316,8 +357,8 @@ export default {
 			// axios.get('http://localhost:8000/api/prestador', {
 			axios.get('https://service.encontrei.online/api/prestador', {
 				headers: {
-						'Content-Type': 'application/json',
-						Authorization: "Bearer " + this.user.token
+					'Content-Type': 'application/json',
+					Authorization: "Bearer " + this.user.token
 				}
 			})
 			.then(response => {
@@ -384,7 +425,34 @@ export default {
 		formatPrice(value) {
 			let val = (value/1).toFixed(2).replace('.', ',')
 			return 'R$ ' + val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
-		}
+		},
+
+		// showAlert() {
+		// 	// this.$swal.fire({
+		// 	// 	title: "Are you sure?",
+		// 	// 	text: "Once deleted, you will not be able to recover this imaginary file!",
+		// 	// 	icon: "warning",
+		// 	// 	buttons: true,
+		// 	// 	dangerMode: true,
+		// 	// })
+		// 	// .then((willDelete) => {
+		// 	// 	if (willDelete) {
+		// 	// 		this.$swal.fire("Poof! Your imaginary file has been deleted!", {
+		// 	// 			icon: "success",
+		// 	// 		});
+		// 	// 	} else {
+		// 	// 		this.$swal.fire("Your imaginary file is safe!");
+		// 	// 	}
+		// 	// });
+		// 	// // // Use sweetalert2
+		// 	// // this.$swal('Hello Vue world!!!');
+
+		// 	// // this.$swal.fire(
+		// 	// //   'Good job!',
+		// 	// //   'You clicked the button!',
+		// 	// //   'success'
+		// 	// // )
+		// },
 	}
 };
 </script>
@@ -392,7 +460,6 @@ export default {
 <style>
 	#loading{
 		position: fixed;
-		background: rgba(40, 167, 69, 0.3);
 		width: 100%;
 		height: 100%;
 		z-index: 9999;
@@ -408,15 +475,15 @@ export default {
 		box-sizing: border-box;
 		display: block;
 		position: absolute;
-		width: 64px;
-		height: 64px;
+		width: 25px;
+		height: 25px;
 		margin: 8px;
-		border: 8px solid #fff;
+		border: 4px solid #fff;
 		border-radius: 50%;
 		animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
 		border-color: #178a31 transparent transparent transparent;
 		left: 50vw;
-		top: 40%;
+		/*top: 40%;*/
 	}
 	.lds-ring div:nth-child(1) {
 		animation-delay: -0.45s;
--- src/main.js
+++ src/main.js
@@ -1,19 +1,20 @@
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import BootstrapVue from 'bootstrap-vue'
-import VueMask from 'di-vue-mask'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import BootstrapVue from 'bootstrap-vue';
+import VueMask from 'di-vue-mask';
+import VueSweetalert2 from 'vue-sweetalert2';
 
 import '../node_modules/bootstrap-vue/dist/bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-
-
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 
 Vue.use(BootstrapVue);
 Vue.use(VueMask);
 Vue.config.productionTip = false
+Vue.use(VueSweetalert2);
 
 /* eslint-disable no-new */
 new Vue({
